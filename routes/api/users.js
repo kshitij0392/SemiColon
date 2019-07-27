d864aa309c386ec6528803d2334f2851a9c12706
Rangari, Kshitij <Kshitij.Rangari@viacomcontractor.com>
2019-07-27 15:42:47 -0400

    setup models for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
-// @route   GET api/users
-// @desc    Test Route
+const {check, validationResult} = require ('express-validator');
+// @route   POST api/users
+// @desc    Register User
 // @Access  Public
-router.get('/', (req, res) => res.send('User Route') ); 
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check ('email', 'Please Include a valid Email').isEmail(),
+    check ('password', 'Please Enter a Password with at least 6 Characters').isLength({min:6})
+],(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array() })
+    }
+    res.send('User Route')
+} ); 
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const connectDB = require ('./config/db');
 const app = express ();
 //Connect to DB
 connectDB();
+//Init Middleware
+app.use(express.json({extended : false}));
 app.get('/',(req,res)=> res.send('API Running'));
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
